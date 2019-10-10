45e805548973afd9b40f10422e3410758c63d4f5
LAPTOP-SAM4GOO0\HP <2307953765@qq.com>
2019-10-10 14:41:23 +0800

    no message

---
+++ vuepoject01/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <fieldset>
+      <legend>
+        <h2>这是header</h2>
+        <router-link :to="{}">购物车</router-link>
+        <router-link :to="{}">个人中心</router-link>
+        <router-link :to="{}">订单信息</router-link>
+      </legend>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
