31d1bdb0b14fd5aa047a007ae98392166582c7ed
DESKTOP-D755EV6\hys15 <15738377452@163.com>
2019-10-10 15:46:02 +0800

    no message

--- vuetwo/src/components/Header.vue
+++ vuetwo/src/components/Header.vue
@@ -4,6 +4,7 @@
         <h2>这是谁的头！</h2>
         <router-link :to="{path:'/home'}">首页</router-link>
         <router-link :to="{path:'/center'}">个人中心</router-link>
+        <router-link :to="{path:'/dd'}">订单信息</router-link>
       </legend>
     </fieldset>
 </template>
--- vuetwo/src/router/index.js
+++ vuetwo/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Center from '../components/center'
 import Home from '../components/home'
+import Dd from '../components/DingDan'
 
 Vue.use(Router)
 
@@ -9,6 +10,7 @@ export default new Router({
   routes: [
     {path:"/home",component:Home},
     {path:"/center",component:Center},
+    {path:"/dd",component:Dd},
     {path:"*",component:Home}
   ]
 })
