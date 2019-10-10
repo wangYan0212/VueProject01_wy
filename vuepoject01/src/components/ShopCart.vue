3fe9adb7e3d25d87216d684cb08495330d3b5984
DESKTOP-57I3R1N\王妍 <www7899898@qq.com>
2019-10-10 14:53:23 +0800

    no message

---
+++ vuepoject01/src/components/ShopCart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="wrap">
+      <h2>购物车页面</h2>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "ShopCart"
+    }
+</script>
+
+<style scoped>
+#wrap{
+  width: 100%;
+  background: yellowgreen;
+}
+</style>
