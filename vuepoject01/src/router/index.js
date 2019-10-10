4eca2ff11a304b62d943191f86340d0245c17213
DESKTOP-D755EV6\hys15 <15738377452@163.com>
2019-10-10 15:04:55 +0800

    Router

--- vuepoject01/src/router/index.js
+++ vuepoject01/src/router/index.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import cart from '../components/ShopCart'
+import center from '../components/center'
+import dd from '../components/DingDan'
 
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [
+    {path:"/cart",component:cart},
+    {path:"/center",component:center},
+    {path:"/dd",component:dd},
+    {path:"*",redirect:"/center"}
+
+  ]
 })
