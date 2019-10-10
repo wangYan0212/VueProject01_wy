7f967a84500537b5de2c95eaab2a2b334a46d985
LAPTOP-SAM4GOO0\HP <2307953765@qq.com>
2019-10-10 15:30:50 +0800

    no message

--- vuetwo/src/components/Header.vue
+++ vuetwo/src/components/Header.vue
@@ -2,6 +2,8 @@
     <fieldset>
       <legend>
         <h2>这是谁的头！</h2>
+        <router-link :to="{path:'/home'}">首页</router-link>
+        <router-link :to="{path:'/center'}">个人中心</router-link>
       </legend>
     </fieldset>
 </template>
--- vuetwo/src/router/index.js
+++ vuetwo/src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Center from '../components/center'
+import Home from '../components/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:"/home",component:Home},
+    {path:"/center",component:Center},
+    {path:"*",component:Home}
   ]
 })
